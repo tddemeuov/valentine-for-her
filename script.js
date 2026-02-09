24ee07c8231a49f4165d9fe84df8be6415f3e3c9
tddemeuov <tddemeuov@kassa24.kz>
2026-02-09 22:47:07 +0500

    Update for check

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
 console.log("SCRIPT LOADED");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const noBtn = document.getElementById("noBtn");
-  const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
 
-  const questionScreen = document.getElementById("questionScreen");
-  const loveScreen = document.getElementById("loveScreen");
+console.log("noBtn:", noBtn);
+console.log("yesBtn:", yesBtn);
 
-  if (!noBtn || !yesBtn) {
-    console.error("Кнопки не найдены");
-    return;
-  }
+noBtn.addEventListener("mouseenter", () => {
+  console.log("NO BUTTON HOVER");
 
-  noBtn.addEventListener("mouseenter", () => {
-    console.log("NO BUTTON RUNNING");
-    const parent = noBtn.parentElement;
-    const maxX = parent.clientWidth - noBtn.offsetWidth;
-    const maxY = parent.clientHeight - noBtn.offsetHeight;
+  const parent = noBtn.parentElement;
+  const maxX = parent.clientWidth - noBtn.offsetWidth;
+  const maxY = parent.clientHeight - noBtn.offsetHeight;
 
-    const x = Math.random() * maxX;
-    const y = Math.random() * maxY;
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
 
-    noBtn.style.left = `${x}px`;
-    noBtn.style.top = `${y}px`;
-  });
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
 
-  yesBtn.addEventListener("click", () => {
-    questionScreen.classList.remove("active");
-    setTimeout(() => {
-      loveScreen.classList.add("active");
-    }, 300);
-  });
+yesBtn.addEventListener("click", () => {
+  alert("Ты моя валентинка ❤️");
 });
